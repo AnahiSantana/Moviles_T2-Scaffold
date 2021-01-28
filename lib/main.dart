9fe287e7ea6dad79fa1df21e81ebcba5f5396001
Anahi S <is715773@iteso.mx>
2021-01-27 20:30:39 -0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: NewWidget(),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tarea1'),
+      ),
+      body: Column(
+        children: [
+          Image.asset("assets/iteso.jpg")
+        ],
+      ),
+    );
+  }
+}
